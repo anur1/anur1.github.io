5cd300ba4603c29d7b954731b2b391252eaead5f
ahmet <ahmettonuri@gmail.com>
2025-03-16 22:08:33 -0500

    css revised, cards revised

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 //Listen if Html page is loaded, if loaded call menuItems 
 window.addEventListener('DOMContentLoaded', function () {
     function4_menuItems();
+    function4_homePage();
 });
 
 // Menu 
@@ -50,11 +51,53 @@ function function4_menuItems() {
 
             });
 
+        })
+        .catch(error => console.error('Error:', error));
+}
 
 
+function function4_homePage() {
+    fetch('./data/menu/home.json')
+        .then(response => response.json())
+        .then(data => {
+            const mainCardContainer = document.getElementById('main-card-container');
+
+            data.sections.forEach(item => {
+                const cardElement = document.createElement('div');
+                const cardBackgroundColor = item.color; // Set background color
+                console.log(cardBackgroundColor);
+                cardElement.innerHTML = `
+
+                <div id="${item.id}" class="card bg-info text-dark content-card mt-5 zoom-effect custom-card" style="background-color:${cardBackgroundColor} !important">
+                    <div class="card-body">
+
+                        <div class="row">
+                            <div class="col-md-3 d-flex align-items-center">
+                                <!-- Image for the first column -->
+                                <img src="${item.image.src}"alt="Smart City Digital Twin"
+                                    class="img-fluid custom-image ">
+                            </div>
+
+                            <div class="col-md-7" id = "card-info-container">
+                                <!-- Content for the second column -->
+                                <h2>${item.content.title}</h2>
+                                <p>${item.content.paragraphs}</p>
 
 
+                            </div>
+                            <div class="col-md-2">
+                                <!-- Keywords for the third column -->
+                                <p><strong>Keywords: </strong>${item.content.keywords}</p>
 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                `;//end of cardElement.innerHTML
+
+                //add above card element to the main card container
+                mainCardContainer.appendChild(cardElement);
+            });
 
         })
         .catch(error => console.error('Error:', error));
